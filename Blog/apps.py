6f69ccd365fbf2b9a99ad03b96b9617dcb282f2f
Sowmiya Muralikumar <sowmiyakmk@gmail.com>
2025-12-17 20:52:28 +0530

    Admin user creation

--- Blog/apps.py
+++ Blog/apps.py
@@ -3,3 +3,24 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'Blog'
+
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Blog"
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        from django.db.utils import OperationalError
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="admin123"
+                )
+        except OperationalError:
+            pass
